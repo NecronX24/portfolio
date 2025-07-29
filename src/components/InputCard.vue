5c7a83ba9ce98f78d6101206a8d6f767d7a595f9
Sebangel <angeljavicueche@gmail.com>
2025-07-29 17:34:58 -0400

    feat: Input Card component created

---
+++ src/components/InputCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class = "inputCard">
+        <slot>
+        </slot>
+    </div>
+</template>
+
+<style scoped>
+    .inputCard {
+        position: fixed;       
+        top: 50%;             
+        left: 50%;            
+        transform: translate(-50%, -50%); 
+        background-color: white;
+        padding: 20px;
+        box-shadow: 0 0 10px rgba(0,0,0,0.2);
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
