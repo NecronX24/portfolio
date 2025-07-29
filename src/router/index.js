3e14d93461c308d4d8f6705cb68c6006a4341166
Sebangel <angeljavicueche@gmail.com>
2025-07-29 16:50:32 -0400

    feat: inital commit with router included

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Home from "@/pages/Home.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+        path: "/",
+        name: "home",
+        component: Home
+    },
+]
+})
+
+export default router;
