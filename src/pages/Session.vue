d71acc358a3e0a8983119d3a50ee67c632f9df65
Sebangel <angeljavicueche@gmail.com>
2025-07-30 19:21:08 -0400

    feat: session page can change between Log In and Sign Up

--- src/pages/Session.vue
+++ src/pages/Session.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Input from "@/components/Input.vue"
     import InputCard from "@/components/InputCard.vue"
+    import Button from "@/components/Button.vue"
     import {ref} from "vue"
 
     const login = [
@@ -15,7 +16,10 @@
         ["Repetir Contraseña", "password"]
     ]
 
-    let state = ref("Sign Up"); 
+    let state = ref("Log In"); 
+    let test = () =>{
+        alert('Hello World')
+    }
 </script>
 
 <template>
@@ -27,8 +31,30 @@
          <template v-else-if="state == 'Sign Up'">
             <Input v-for="(item, index) in signup" :key="index" :span-text="item[0]" :type="item[1]"/>
         </template>
+        <Button :event="test" :text="state == 'Log In'? 'Log In' : 'Sign Up'"/>
+        <p>
+            {{ state == 'Log In' ? '¿No tienes una cuenta?' : '¿Ya tienes una cuenta?' }}
+            <a @click="state == 'Log In' ? state='Sign Up' : state='Log In'">
+                {{ state == 'Log In' ? 'Regístrate' : 'Inicia sesión' }}
+            </a>
+        </p>
     </InputCard>
 </template>
 
 <style scoped>
+p {
+    margin-top: 20px;
+    color: #808080;
+}
+
+p a {
+    color: #2575fc;
+    cursor: pointer;
+    text-decoration: underline;
+}
+
+p a:hover {
+    color: #1a4fd8;
+}
+
 </style>
