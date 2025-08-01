6f827f2ef51ce61001569dad09d36c10ae192600
Sebangel <angeljavicueche@gmail.com>
2025-08-01 17:32:40 -0400

    feat: styles and content added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,55 @@
 <script setup>
+import Button from '@/components/Button.vue';
 import Navbar from '@/components/Navbar.vue';
 
 let elements =[
-    ["Hola", ()=>{alert('Hola')}]
+    ["Home", ()=>{alert('Hola')}],
+    ["About", ()=>{alert('Hola')}]
 ]
 </script>
 
 <template>
     <Navbar :elements="elements"></Navbar>
-    <h1>Hello World</h1>
+    <div class="home">
+        <div class="home-text">
+            <h1 class="home-title">Hello!, I'm Angel Cueche</h1>
+            <h2 class="home-subtitle">Junior Developer</h2>
+            <p class="home-p">
+                Computer Engineering's Student
+            </p>
+            <p class="home-p"> A passionate person who is looking for real experience in the programming world!</p>
+        </div>
+    </div>
+    <div class="projects">
+        <div>Hello Wold</div>
+    </div>
 </template>
 
-<styles scoped>
+<style scoped>
+.home{
+    background: linear-gradient(270deg, #08b51f82, #055610db);
+    min-height: 90vh;
+}
+.home-text{
+    padding-top:4%;
+    padding-left:7%;
+    width: 30%;
+    color:white;
+}
 
-</styles>
+.home-title {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+
+.home-subtitle {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  opacity: 0.9;
+}
+
+.home-p {
+  font-size: 1.2rem;
+  line-height: 1.6;
+}
+</style>
