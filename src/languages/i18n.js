515c545594449bf5ce5e68a196165748bbf03fea
Sebangel <angeljavicueche@gmail.com>
2025-09-01 22:17:02 -0400

    chore: i18n edited to add spanish

--- src/languages/i18n.js
+++ src/languages/i18n.js
@@ -2,13 +2,13 @@ import {createI18n} from 'vue-i18n';
 import en from '../languages/en.json';
 import es from "../languages/es.json";
 const messages = {
-    en
+    en, es
 }
 
 const i18n = createI18n({
     legacy:false,
-    local:'en', //idioma por defecto
-    // fallbackLocale: 'es', //para mostrar traducción en caso de que no esté en el idioma seleccionado
+    locale:'en', //idioma por defecto
+     fallbackLocale: 'es', //para mostrar traducción en caso de que no esté en el idioma seleccionado
     messages
 })
 
